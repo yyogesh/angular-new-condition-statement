91c0dd5994301a42b7a25ed742f5065ffe599699
yyogesh <yogeshggn@hotmail.com>
2024-06-14 14:35:23 +0530

    angular if for switch and empty block

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
+import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'my-app';
+  showOption = false;
+  selectionOption = "Angular"
+
+  user$ = of('ABC');
+
+  users: any[] = [
+    
+  ];
+
+  toggleOption(): void{
+    this.showOption = !this.showOption;
+  }
 }
